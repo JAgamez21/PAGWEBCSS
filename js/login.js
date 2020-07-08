cf6e2b6fc4ce535b35d49eabd805854800f8fe33
Nestor Cortina <junior110120@gmail.com>
2020-07-07 20:29:02 -0500

    logica de la session

--- js/login.js
+++ js/login.js
@@ -27,3 +27,30 @@ function eventListeners() {
   //Cuando se envia el formulario
   document.getElementById('formlogin').addEventListener('submit', formlogin);
 }
+
+function formlogin(e) {
+  //Prevenimos el envio del formulario
+  e.preventDefault()
+
+  const user = document.getElementById('user').value
+  const password = document.getElementById('password').value
+
+  let login = false
+  for (let i = 0; i < usuarios.length; i++) {
+    if (usuarios[i].user === user && usuarios[i].password === password) {
+      setUserSessionStorage(usuarios[i])
+      login = true
+      break;
+    }
+  }
+
+  if (login) {
+    location.href = '/index.html'
+  } else {
+    alert('User/password incorrect')
+  }
+}
+
+function setUserSessionStorage(user) {
+  sessionStorage.setItem('user', JSON.stringify(user))
+}
